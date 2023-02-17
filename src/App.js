682e90aa3319477f1a1af9e3eec54a695ceb1eb3
Guitti <guilhermederetti9@gmail.com>
2023-02-17 15:54:00 +0000

    added base components and structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import MemoryGame from './components/pages/MemoryGame';
+import Scores from './components/pages/Scores';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="container">        
+        <Routes>
+          <Route path="memory-giphy-game/" element={<MemoryGame />} />
+          <Route path="memory-giphy-game/scores" element={<Scores />} />
+        </Routes>
+        <Nav />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+/*
+
+Load all cards from array/json
+  cards have ("Id" which can be duplicated) ("UniqueKey") ("url/src" for image) ("flipped" true or false)
+  card{
+    
+    flip(){
+      flipped = !flipped;
+    }
+
+    onClick (){
+      add card to selectedArr
+      card.flip()// show it
+      if selectedArr.length == 2 // check if have 2 card's selected{
+        if(selectedArr[0].id == selectedArr[1].id){
+          matchArr = matchArr.concat(selectedArr);// add selected to the match array
+          score++;
+        } else { // hide cards if not equals
+          timer(1000ms){
+            selectedArr[0].flip();
+            selectedArr[1].flip();
+            selectedArr = []; 
+            score--;
+          }
+        }
+      }
+    }
+  }
+
+
+*/
---
+++ src/components/pages/Card.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+function Card(props) {
+    const [idState, setIdState] = useState(0);
+    const [flippedState, setFlippedState] = useState(false);
+    const [imageState, setImageState] = useState("https://picsum.photos/id/1/250/250");
+
+    useEffect(() => {
+        {flippedState ? setImageState(props.image) : setImageState("https://picsum.photos/id/1/250/250")}
+     }, [flippedState]);
+
+    return (
+        <div className="col-sm"> 
+            <img src={(imageState)} onClick={() => setFlippedState(!flippedState)}/>      
+        </div>      
+    );
+}
+
+export default Card;
