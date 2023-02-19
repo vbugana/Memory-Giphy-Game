6821a1cba37e8e1926dbc0689b18ec2f8d483c82
Santiago <sagos0919@gmail.com>
2023-02-19 18:16:07 +0000

    custom theme created and added font

---
+++ src/utils/theme/index.jsx
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+	fonts: {
+		heading: `'Press Start 2P', cursive`,
+		body: `'Press Start 2P', cursive`,
+	},
+});
+
+export default theme;
